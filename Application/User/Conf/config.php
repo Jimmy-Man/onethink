768975a73294db78b6c6baf5c04cac68e8c0462c
Office <1877876812@qq.com>
2015-09-21 11:58:20 +0800

    添加框架文件

---
+++ Application/User/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+/**
+ * UCenter客户端配置文件
+ * 注意：该配置文件请使用常量方式定义
+ */
+
+define('UC_APP_ID', 1); //应用ID
+define('UC_API_TYPE', 'Model'); //可选值 Model / Service
+define('UC_AUTH_KEY', 'gMK(Q.L%a=PiEf*8)Nl5_"{OF?2Ytqr$B}S!ReZ['); //加密KEY
+define('UC_DB_DSN', 'mysqli://onethink:mQJqqvAjyH7uM98U@127.0.0.1:3306/onethink'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', 'onethink_'); // 数据表前缀，使用Model方式调用API必须配置此项
